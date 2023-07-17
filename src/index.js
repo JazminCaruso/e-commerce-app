0d6f2d5146a6538f0e20eb5b04749b9a17cdf040
Jazmin <carusojazmin@gmail.com>
2023-07-17 18:25:29 -0300

    RootNavigator en index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import { useFonts } from 'expo-font';
-import { ActivityIndicator, SafeAreaView, StyleSheet, View, Text, Platform, StatusBar } from 'react-native';
-import { Header } from './components/components';
-import { Categories, Products } from './screens/screens'
-import { useState } from 'react';
-import { COLORS, FONTS } from './themes/themes';
+import { ActivityIndicator, SafeAreaView, StyleSheet, View, Platform, StatusBar } from 'react-native';
 
-const categoryDefault = {
-  categoryId: null,
-  categoryColor: COLORS.background,
-  categoryName: null,
-};
+import { COLORS, FONTS } from './themes/themes';
+import RootNavigator from './navigation/index'
+
 
 export default function App() {
 
@@ -19,21 +13,6 @@ export default function App() {
     [FONTS.medium]: require('../assets/fonts/YsabeauInfant-Medium.ttf'),
     [FONTS.regular]: require('../assets/fonts/YsabeauInfant-Regular.ttf'),
   })
-
-  const [isCategorySelected, setIsCategorySelected] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState(categoryDefault);
-
-  const onHandlerSelectCategory = ({ categoryId, categoryColor, categoryName }) => {
-    setSelectedCategory({ categoryId, categoryColor, categoryName });
-    setIsCategorySelected(!isCategorySelected);
-  }
-
-  const onHandlerNavigate = () => {
-    setIsCategorySelected(!isCategorySelected);
-    setSelectedCategory(categoryDefault);
-  }
-
-  const headerTitle = isCategorySelected ? `Libros de ${selectedCategory.categoryName}` : 'Categorías';
 
   if(!loaded) {
     return (
@@ -45,25 +24,7 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        {!isCategorySelected && 
-          <View>
-            <Text style={styles.principalText}>LibrApp</Text>
-            <Text style={styles.subtitleText}>Descubre, elige y disfruta de los mejores libros</Text>
-          </View>
-        }
-        <Header 
-          title={headerTitle}
-          style={{ backgroundColor: selectedCategory.categoryColor }}
-        />
-        {
-          isCategorySelected ? (
-            <Products onHandlerGoBack={onHandlerNavigate} categorySelected={selectedCategory}/>
-          ) : (
-            <Categories onSelectCategory={onHandlerSelectCategory}/>
-          )
-        }
-      </View>
+      <RootNavigator />
     </SafeAreaView>
   );
 }
@@ -77,20 +38,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  principalText: {
-    margin: 10,
-    textAlign: 'center',
-    fontFamily: FONTS.bold,
-    fontSize: 32,
-    color: COLORS.primary,
-  }, 
-  subtitleText: {
-    marginHorizontal: 10,
-    marginBottom: 15,
-    textAlign: 'center',
-    fontFamily: FONTS.italic,
-    fontSize: 20,
-    color: COLORS.primary,
   },
 });
