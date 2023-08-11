6640f13fb78a2062ae4c5d9afd5ca49f051b845b
Jazmin <carusojazmin@gmail.com>
2023-08-10 23:45:31 -0300

    authentication

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,8 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = 'https://e-commercelibros-default-rtdb.firebaseio.com';
+export const FIREBASE_API_KEY = 'AIzaSyCpCghO7MIdHQsouc4g7UNqHZllzp_1vVw';
+export const FIREBASE_AUTH_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts';
+export const FIREBASE_AUTH_SIGN_IN_API_URL = `:signInWithPassword?key=${FIREBASE_API_KEY}`;
+export const FIREBASE_AUTH_SIGN_UP_API_URL = `:signUp?key=${FIREBASE_API_KEY}`;
+
+
+
--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,11 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 
 import TabsNavigator from "./tabs";
+import AuthNavigator from "./auth";
+import { useState } from "react";
 
 function RootNavigator() {
+    const [userId, setUserId] = useState(null);
     return (
         <NavigationContainer>
-            <TabsNavigator />
+            {userId ? <TabsNavigator /> : <AuthNavigator />}
         </NavigationContainer>
     );
 }
