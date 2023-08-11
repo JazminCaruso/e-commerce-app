0b200208ce9e346d2929a7322dcb5ff03d0aef52
Jazmin <carusojazmin@gmail.com>
2023-08-11 15:37:20 -0300

    conexion camara

--- src/components/imageSelector/index.jsx
+++ src/components/imageSelector/index.jsx
@@ -1,12 +1,38 @@
-import { View, TouchableOpacity, Text, Image } from "react-native";
+import { View, TouchableOpacity, Alert, Image } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from "./styles";
 import { COLORS } from "../../themes/colors";
+import { requestMediaLibraryPermissionsAsync, launchImageLibraryAsync } from 'expo-image-picker';
 
-const ImageSelector = ({ image, onSelect }) => {
+const ImageSelector = ({ image }) => {
+
+    const verifyPermissions = async () => {
+        const { status } = await requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+            Alert.alert(
+                'Permiso denegado',
+                'Se necesita permiso para que la aplicación pueda funcionar',
+                [{ text: 'Ok' }, ]
+            );
+            return false;
+        }
+        return true;
+    };
+
+    const onHandlerTakePhoto = async () => {
+        const isMediaPermission = await verifyPermissions();
+        if (!isMediaPermission) return;
+        const result  = await launchImageLibraryAsync ({
+            mediaTypes: 'Images',
+            allowsEditing: true,
+            aspect: [12, 12],
+            quality: 0.5,
+        })
+    };
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={onSelect} style={styles.content} >
+            <TouchableOpacity onPress={onHandlerTakePhoto} style={styles.content} >
                 { image ? (
                     <Image source={{ uri: image }} style={styles.image} />
                 ) : (
