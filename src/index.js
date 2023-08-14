dc075347b958bc34c7540fb3ac465dec75f08ea5
Jazmin <carusojazmin@gmail.com>
2023-08-14 20:07:18 -0300

    crear tabla

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { COLORS, FONTS } from './themes/themes';
 import RootNavigator from './navigation/index'
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { init } from './db';
+
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch((err) => {
+    console.log('Initializing db failed');
+    console.log(err);
+  })
 
 export default function App() {
 
